9ad1176974747eca6434f1f016e9626df3b6a731
hardyliu <hardyliu@tencent.com>
2019-11-13 23:15:41 +0800

    feat: content

--- config/mydata.php
+++ config/mydata.php
@@ -49,5 +49,194 @@ $items = array(
         'trackArtist'=> '欢乐谷第一单元',
         'trackAlbum' => 'Single'
     ),
+    array(
+        'file' => 'tracks/hanyupinyin1.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '汉语拼音1',
+        'trackArtist'=> '欢乐谷第二单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin2.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '汉语拼音2',
+        'trackArtist'=> '欢乐谷第二单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin3.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '汉语拼音3',
+        'trackArtist'=> '欢乐谷第二单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin4.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '汉语拼音4',
+        'trackArtist'=> '欢乐谷第二单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin5.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '汉语拼音5',
+        'trackArtist'=> '欢乐谷第二单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin6.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '汉语拼音6',
+        'trackArtist'=> '欢乐谷第二单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin7.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '汉语拼音7',
+        'trackArtist'=> '欢乐谷第三单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin8.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '汉语拼音8',
+        'trackArtist'=> '欢乐谷第单三元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin9.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '汉语拼音9',
+        'trackArtist'=> '欢乐谷第三单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin10.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '汉语拼音10',
+        'trackArtist'=> '欢乐谷第三单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin11.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '汉语拼音11',
+        'trackArtist'=> '欢乐谷第三单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin12.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '汉语拼音12',
+        'trackArtist'=> '欢乐谷第三单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/hanyupinyin13.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '汉语拼音13',
+        'trackArtist'=> '欢乐谷第三单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/qiutianxie.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '秋天笔顺',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/qiutiandu.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '秋天朗读',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/qiutianting.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '秋天听写',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/xiaoxiaodechuanxie.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '小小的船笔顺',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/xiaoxiaodechuandu.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '小小的船朗读',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/xiaoxiaodechuanting.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '小小的船听写',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/jiangnanxie.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '江南笔顺',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/jiangnandu.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '江南朗读',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/jiangnanting.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '江南听写',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/sijixie.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '四季笔顺',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/sijidu.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> '四季朗读',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/sijiting.mp3',
+        'thumb'=> 'thumbs/01.jpg',
+        'trackName'=> '四季听写',
+        'trackArtist'=> '欢乐谷第四单元',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/1am1u1.mp3',
+        'thumb'=> 'thumbs/02.jpg',
+        'trackName'=> '1AM1U1',
+        'trackArtist'=> '牛津英语(朗读纸)',
+        'trackAlbum' => 'Single'
+    ),
+    array(
+        'file' => 'tracks/asong.mp3',
+        'thumb'=> 'thumbs/03.jpg',
+        'trackName'=> 'Aa儿歌',
+        'trackArtist'=> '英语儿歌',
+        'trackAlbum' => 'Single'
+    ),
 );
 ?>
